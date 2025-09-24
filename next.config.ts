529b754993685f649ea44bcda506aec0caf2c772
John Doe <maxpain45545@gmail.com>
2025-09-24 19:53:14 +0530

    minor fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Produce a minimal standalone server for Docker
   output: 'standalone',
+  images: {
+    domains: [
+      'corpbiz.io',
+      'images.unsplash.com',
+    ],
+  },
 };
 
 export default nextConfig;
